5223d499e6e5a14021ad9dcdcfa4b853110f59d1
Tony Xin <xyytonyxin@gmail.com>
2021-10-09 04:04:44 -0700

    Updated

--- src/Components/CollegeDashboard/CollegeDashboard.jsx
+++ src/Components/CollegeDashboard/CollegeDashboard.jsx
@@ -3,7 +3,8 @@ import { Link, useHistory } from "react-router-dom";
 import { auth , logout } from "../../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { StudentBox } from "./StudentBox"
-import { Text, Badge } from "@chakra-ui/react"
+import { Text, Badge, Box, HStack, Tag } from "@chakra-ui/react"
+import './CollegeDashboard.css'
 
 function CollegeDashboard() {
   const [user, loading, error] = useAuthState(auth);
@@ -16,11 +17,72 @@ function CollegeDashboard() {
     }
     if (!user) history.replace("/signin");
   }, [user, loading]);
+
+  let clientData = [
+    {
+      imageUrl: "https://i.imgur.com/HkLY72h.jpg",
+      firstName: "Tony",
+      lastName: "Xin",
+      highschool: "Mountain View High School",
+      grade: "Freshman",
+      major: "Physics"
+    },
+    {
+      imageUrl: "https://i.imgur.com/HkLY72h.jpg",
+      firstName: "Tony",
+      lastName: "Xin",
+      highschool: "Mountain View High School",
+      grade: "Freshman",
+      major: "CS"
+    },
+    {
+      imageUrl: "https://i.imgur.com/HkLY72h.jpg",
+      firstName: "Tony",
+      lastName: "Xin",
+      highschool: "Mountain View High School",
+      grade: "Freshman",
+      major: "Philosophy"
+    },
+    {
+      imageUrl: "https://i.imgur.com/HkLY72h.jpg",
+      firstName: "Tony",
+      lastName: "Xin",
+      highschool: "Mountain View High School",
+      grade: "Freshman",
+      major: "History"
+    },
+    {
+      imageUrl: "https://i.imgur.com/HkLY72h.jpg",
+      firstName: "Brendan",
+      lastName: "Wong",
+      highschool: "Mountain View High School",
+      grade: "Freshman",
+      major: "Math"
+    },
+  ]
+
+  const clientItems = clientData.map((client) => 
+  <StudentBox imageUrl={client.imageUrl} firstName={client.firstName} lastName={client.lastName} highschool={client.highschool} grade={client.grade} major={client.major}></StudentBox>
+  )
+
   return (
     <div className="dashboard">
-        <Text m={5} fontSize="6xl">Welcome, Tony 👋</Text>
-        <Text m={5} fontSize="4xl">Your balance is <Badge mb={0.5} fontSize="0.8em">{ balance }</Badge></Text>
-        <StudentBox></StudentBox>
+        <Text fontSize="6xl">Welcome, Tony 👋</Text>
+        <Text mt={5} fontSize="4xl">Your balance is <Badge mb={0.5} fontSize="0.8em" colorScheme="green">{ balance }</Badge></Text>
+        
+        <Box borderRadius="md" borderWidth="1px" p={4} mt="4">
+            <Tag fontSize="3xl" colorScheme="gray" mb={2}>Clients</Tag>
+            <HStack spacing="10px">
+                {clientItems}
+            </HStack>
+        </Box>
+
+        <Box borderRadius="md" borderWidth="1px" p={4} mt="4">
+            <Tag fontSize="3xl" colorScheme="gray" mb={2}>Connections</Tag>
+            <HStack spacing="10px">
+                {clientItems}
+            </HStack>
+        </Box>
     </div>
   );
 }
