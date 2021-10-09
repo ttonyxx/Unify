b56ebd2f010393fb3b16c65803e9d68658bd3002
Caleb Kim <calebkim32@gmail.com>
2021-10-08 17:10:20 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <router>
+      <div>
+        <Nav/>
+
+        <Route path="/signin" component={SignIn}/>
+        <Route path="/dashboard" component={StudentDashboard}/>
+        
+      </div>
+    </router>
   );
 }
 
