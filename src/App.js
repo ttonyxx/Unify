086dac8bda994ea3c0aed5302867034a85ac802c
Caleb Kim <calebkim32@gmail.com>
2021-10-08 17:31:06 -0700

    RouteTemplate(Fixed) + BrowserRouter wrap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import StudentDashboard from './Components/StudentDashboard'
+import SignIn from './Components/SignIn'
+import { Route , Switch } from "react-router-dom";
 
 function App() {
   return (
-    <router>
-      <div>
-        <Nav/>
-
-        <Route path="/signin" component={SignIn}/>
-        <Route path="/dashboard" component={StudentDashboard}/>
-        
-      </div>
-    </router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+        <Switch>
+          <Route path="/signin" component={SignIn}/>
+          <Route path="/dashboard" component={StudentDashboard}/>
+        </Switch>
+    </div>
   );
 }
 
---
+++ src/Components/StudentDashboard.jsx
@@ -0,0 +1,3 @@
+export default function StudentDashboard() {
+    return <h1>Hello</h1>;
+  }
