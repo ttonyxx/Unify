cddcffc9880511660dbc4c965743ba9065a0733e
Brendan Wong <35351983+LunrEclipse@users.noreply.github.com>
2021-10-09 04:07:13 -0700

    Rough Draft for Profile

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Text, Box, Tag, Image, Flex, Stack, Badge, Button} from "@chakra-ui/react"
-import { StarIcon } from "@chakra-ui/icons"
+import { Text, Box, Tag, Image, Flex, Stack, Badge, Button, HStack, Divider} from "@chakra-ui/react"
+import { StarIcon, EmailIcon, ArrowForwardIcon } from "@chakra-ui/icons"
 import getUser from '../../utils';
 import { UserRefreshClient } from "google-auth-library";
 
@@ -23,8 +23,8 @@ function Profile() {
     });
 
     return (
-        <div className="profile">
-            <Flex
+        <Flex className="profile" height='100%'>
+            <Flex 
                 flexDirection='row'
                 justifyContent='left'
                 alignItems='center'
@@ -36,7 +36,7 @@ function Profile() {
                 >
                     <Image
                         boxSize="300px"
-                        margin='0rem 2rem 1rem 0rem'
+                        margin='1rem 2rem 1rem 0rem'
                         src={user.imageUrl}
                         fallbackSrc="https://via.placeholder.com/150"
                     />
@@ -52,12 +52,45 @@ function Profile() {
                     <Text fontSize="1xl" align = "center" margin='0.15rem 0rem 0.15rem 0rem'>with excellent knowledge about</Text>
                     <Text fontSize="1xl" align = "center" margin='0.15rem 0rem 0.15rem 0rem'>not only college, but Computer</Text>
                     <Text fontSize="1xl" align = "center" margin='0.15rem 0rem 0.5rem 0rem'>Science as a whole" - Caleb Kim</Text>
-                    <Button colorScheme="teal" size="lg" alignItems = 'center'>
-                        Connect
-                    </Button>
+                    <HStack spacing={4}>
+                        <Button leftIcon={<EmailIcon />} colorScheme="teal" variant="solid">
+                            Message
+                        </Button>
+                        <Button rightIcon={<ArrowForwardIcon />} colorScheme="teal" variant="outline">
+                            Connect
+                        </Button>
+                    </HStack>
                 </Flex>
+                <Box height = "90%">
+                    <Divider orientation="vertical" variant = "solid" borderWidth = "1px" borderColor = "teal"
+                    margin='0rem 2rem 0rem 0rem'/>
+                </Box>
                 <Box>
                     <Text fontSize="6xl" align="left">{user.firstName} {user.lastName}</Text>
+                    <Box>
+                        <Tag fontSize="2xl" colorScheme="cyan" mb={2}>Information</Tag>
+                        <HStack margin='0.5rem 0rem 1rem 0rem'>
+                            <Tag fontSize="large" colorScheme="gray">College:</Tag>
+                            <Text fontSize ="large">{user.college}</Text>
+                        </HStack>
+                        <HStack margin='1rem 0rem 1rem 0rem'>
+                            <Tag fontSize="large" colorScheme="gray">Year:</Tag>
+                            <Text fontSize ="large">{user.year}</Text>
+                        </HStack>
+                        <HStack margin='1rem 0rem 1rem 0rem'>
+                            <Tag fontSize="large" colorScheme="gray">Major:</Tag>
+                            <Text fontSize ="large">{user.major}</Text>
+                        </HStack>
+                        <Tag fontSize="2xl" colorScheme="cyan" mb={2}>Contact Information</Tag>
+                        <HStack margin='0.5rem 0rem 1rem 0rem'>
+                            <Tag fontSize="large" colorScheme="gray">Phone Number:</Tag>
+                            <Text fontSize ="large">{user.mobile}</Text>
+                        </HStack>
+                        <HStack margin='1rem 0rem 1rem 0rem'>
+                            <Tag fontSize="large" colorScheme="gray">Email:</Tag>
+                            <Text fontSize ="large">{user.email}</Text>
+                        </HStack>
+                    </Box>
                     <Text fontSize="1xl">Hi, my name is Tony and I'm a student living in Silicon Valley. I have a passion for creation</Text>
                     <Text fontSize="1xl"> and I'm always looking for outlets to express it. Whether it's coding applications, building businesses,</Text>
                     <Text fontSize="1xl">  or leading projects, I'm constantly fueled by my desire to create. Take a look some of the things I've done below!</Text>
@@ -74,18 +107,9 @@ function Profile() {
                             {user.reviewCount} reviews
                         </Box>
                     </Box>
-                    <Box borderRadius="md" borderWidth="1px" p={4} mt="4" borderColor = 'teal'>
-                        <Tag fontSize="3xl" colorScheme="cyan" mb={2}>Information</Tag>
-                        <Text fontSize="2xl" margin='1rem 0rem 1rem 0rem'>College: {user.college}</Text>
-                        <Text fontSize="2xl" margin='1rem 0rem 1rem 0rem'>Grade: {user.year}</Text>
-                        <Text fontSize="2xl" margin='1rem 0rem 1rem 0rem'>Major: {user.major}</Text>
-                        <Tag fontSize="3xl" colorScheme="cyan" mb={2}>Contact Information</Tag>
-                        <Text fontSize="2xl" margin='1rem 0rem 1rem 0rem'>Phone Number: {user.mobile}</Text>
-                        <Text fontSize="2xl" margin='1rem 0rem 1rem 0rem'>Email: {user.email}</Text>
-                    </Box>
                 </Box>
             </Flex>
-        </div>
+        </Flex>
     );
 }
 export default Profile;
