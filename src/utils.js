fcd20a3ef04ae7221bb9c3eafe5670e38c6c1b96
Brendan Wong <35351983+LunrEclipse@users.noreply.github.com>
2021-10-09 00:15:57 -0700

    Created basic filter functions

---
+++ src/utils.js
@@ -0,0 +1,73 @@
+import Firestore from "@google-cloud/firestore";
+
+const db = new Firestore({
+        projectId: 'unify-ee0a5',
+        keyFilename: 'unify-328418-c8687e44be6e.json',
+    });
+
+function filterMajor(major)
+{
+    const usersRef = db.collection('users');
+    const query = await usersRef
+      .collection("users")
+      .where('major', '==', major)
+      .where('type', '==', 'college')
+      .get();
+    if (query.empty) {
+    console.log('No matching documents.');
+    return;
+    }  
+    else{
+        var array = [];
+        query.forEach(user => {
+            array.push(user);
+          });
+        return array;
+    }
+}
+
+function filterCollege(college)
+{
+    const usersRef = db.collection('users');
+    const query = await usersRef
+      .collection("users")
+      .where('college', '==', college)
+      .where('type', '==', 'college')
+      .get();
+    if (query.empty) {
+    console.log('No matching documents.');
+    return;
+    }  
+    else{
+        var array = [];
+        query.forEach(user => {
+            array.push(user);
+          });
+        return array;
+    }
+}
+
+function filterName(name)
+{
+    const usersRef = db.collection('users');
+    const query = await usersRef
+      .collection("users")
+      .where('type', '==', 'college')
+      .get();
+    if (query.empty) {
+    console.log('No matching documents.');
+    return;
+    }
+    else{
+        var array = [];
+        query.forEach(user => {
+            if(user.name.contains(name))
+            {
+                array.push(user);
+            }
+          });
+        return array;
+    }
+}
+
+export default utils
