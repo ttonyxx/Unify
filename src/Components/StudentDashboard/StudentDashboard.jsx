11ce527633a5b0eb080cff45b1b546bdea58ecb4
Matthew Lee <matthewlee626@gmail.com>
2021-10-09 17:51:33 -0700

    dashboard edits

--- src/Components/StudentDashboard/StudentDashboard.jsx
+++ src/Components/StudentDashboard/StudentDashboard.jsx
@@ -2,38 +2,136 @@ import React, { useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { auth , logout } from "../../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { Flex, Text, Table, Thead, Tbody, Tr, Th, Td, Tag, Select} from "@chakra-ui/react";
+import DatePicker from "react-datepicker";
+import { 
+  Flex, 
+  Text, 
+  Table, 
+  Thead, 
+  Tbody, 
+  Tr, 
+  Th, 
+  Td, 
+  Tag, 
+  Select, 
+  Badge,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Box
+} from "@chakra-ui/react";
+import { AddIcon } from '@chakra-ui/icons'
+
+import "react-datepicker/dist/react-datepicker.css";
 
 const mockStudentData = [
   {
     'name': 'Harvard',
-    'date': '2012-04-23T18:25:43.511Z',
-    'status': 'done'
+    'date': '1640433600',
+    'status': 'Not Started'
   },
   {
     'name': 'Berkeley',
-    'date': '2012-04-23T18:25:43.511Z',
-    'status': 'done'
+    'date': '1634033600',
+    'status': 'Not Started'
   },
   {
-    'name': 'DeWry',
-    'date': '2012-04-23T18:25:43.511Z',
-    'status': 'done'
+    'name': 'Tsinghua',
+    'date': '1635033600',
+    'status': 'Not Started'
+  },
+  {
+    'name': 'DeVry',
+    'date': '1635633600',
+    'status': 'Not Started'
   },
   {
     'name': 'KAIST',
-    'date': '2012-04-23T18:25:43.511Z',
-    'status': 'done'
+    'date': '1638533600',
+    'status': 'Not Started'
   }
-
 ];
 
-function StudentDashboard() {
+const pickSelectColor = value => {
+  switch (value) {
+    case 'Not Started':
+      return 'gray';
+    case 'Started':
+      return 'orange';
+    case 'Drafted':
+      return 'khaki';
+    case 'Done':
+      return 'green';
+    default:
+      return 'white'
+  }
+}
+
+const timeLeftColor = days => {
+  if (days <= 7) {
+    return 'red'
+  } else if (days <= 14) {
+    return 'orange'
+  } else if (days <= 30) {
+    return 'yellow'
+  } else if (days <= 60) {
+    return 'green'
+  } else {
+    return 'gray'
+  }
+}
+
+const collegeAddSubmit = (data) => {
+  console.log(data);
+}
+
+const StatusSelectDropdown = ({ college }) => {
+
+  const [selectColor, setSelectColor] = useState(pickSelectColor(college.status));
+
+  const changeSelectColor = e => {
+    setSelectColor(pickSelectColor(e.target.value))
+  }
+
+  return (
+    <Select 
+      defaultValue={college.status}
+      bg={selectColor}
+      onChange={changeSelectColor}
+      color='white'
+    >
+      {['Not Started', 'Started', 'Drafted', "Done"].map((status) => (
+        <option value={status} style={{'color': 'black'}} >
+          {status}
+        </option>
+      ))}
+    </Select>
+  )
+}
+
+const StudentDashboard = () => {
   // const [email, setEmail] = useState("");
   // const [password, setPassword] = useState("");
   const [user, loading, error] = useAuthState(auth);
+  const [date, setDate] = useState("");
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [newCollegeDueDate, setNewCollegeDueDate] = useState(new Date());
+
   const history = useHistory();
+
   useEffect(() => {
+    setDate(Date.now());
     if (loading) {
       // maybe trigger a loading screen
       return;
@@ -45,7 +143,60 @@ function StudentDashboard() {
       flexDirection='column'
       padding='0 10rem 0 10rem'
     >
-        <Text fontSize="4xl" padding='3rem 0 3rem 0'>Welcome, {user ? user.displayName.split(" ")[0] : ""} 👋</Text>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add College</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+          <FormControl id="collegeInfo">
+            <FormLabel>College Name</FormLabel>
+              <Input type="collegeName" />
+            <FormHelperText>Any college works!</FormHelperText>
+            <Box height='1rem' />
+            <FormLabel>Due Date</FormLabel>
+            <Box 
+              sx={{
+                'borderStyle': 'solid',
+                'borderColor': 'inherit',
+                'borderWidth': '1px',
+                'borderRadius': '5px',
+                'padding': '0.5rem',
+              }}
+            >
+              <DatePicker 
+                selected={newCollegeDueDate} 
+                onChange={(date) => setNewCollegeDueDate(date)} 
+              />
+            </Box>
+            <FormHelperText>Make sure to double check!</FormHelperText>
+            <Button colorScheme="blue" mr={3} onClick={collegeAddSubmit}>
+              Submit
+            </Button>
+          </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      <Text fontSize="4xl" padding='1.5rem 0 1rem 0'>Welcome, {user ? user.displayName.split(" ")[0] : ""} 👋</Text>
+
+      <Button 
+        onClick={onOpen}
+        colorScheme="blue"
+        height="2rem"
+        width="9rem"
+        padding="1.5rem"
+        leftIcon={<AddIcon />}
+        sx={{
+          'alignSelf': 'flex-end',
+          'mb': '1rem'
+        }}
+      >
+        Add College
+      </Button>
+
       <Table>
         <Thead>
           <Tr>
@@ -53,7 +204,10 @@ function StudentDashboard() {
               College
             </Th>
             <Th>
-              Date
+              Due By
+            </Th>
+            <Th>
+              Time Left
             </Th>
             <Th>
               Status
@@ -62,20 +216,24 @@ function StudentDashboard() {
         </Thead>
         <Tbody>
           {mockStudentData.map((college) => {
+            const dueDate = new Date(college.date * 1000)
+            const timeLeft = college.date * 1000 - date
+            const daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24))
             return (
               <Tr>
                 <Td>
                   {college.name}
                 </Td>
                 <Td>
-                  {college.date}
+                  {`${dueDate.toLocaleDateString()}`}
                 </Td>
                 <Td>
-                  <Select placeholder={"teg"} defaultValue={college.status}>
-                    <option value={college.status}>
-                      {college.status}
-                    </option>
-                  </Select>
+                  <Badge colorScheme={timeLeftColor(daysLeft)}>
+                    {`${daysLeft} days left!`}
+                  </Badge>
+                </Td>
+                <Td>
+                  <StatusSelectDropdown college={college} />
                 </Td>
               </Tr>
             )
@@ -85,4 +243,5 @@ function StudentDashboard() {
     </Flex>
   );
 }
+
 export default StudentDashboard;
--- src/utils.js
+++ src/utils.js
@@ -104,7 +104,7 @@ export function getRecommended(user)
 export function shuffle(array) {
   let currentIndex = array.length,  randomIndex;
 
-  while (currentIndex != 0) {
+  while (currentIndex !== 0) {
 
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
